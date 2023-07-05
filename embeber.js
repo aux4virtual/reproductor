e4638a3eec010541cb6d0142a1e0b8575e300f60
StivenRuiz <stiven70@outlook.com>
2023-07-05 16:44:54 -0600

    se ajusta la altura del script embeber

--- embeber.js
+++ embeber.js
@@ -5,7 +5,7 @@ function embedPage() {
   var iframe = document.createElement("iframe");
   iframe.src = "https://aux4virtual.github.io/reproductor/";
   iframe.width = "100%";
-  iframe.height = "500";
+  iframe.height = "270";
   iframe.frameBorder = "0";
   iframe.scrolling = "no";
   embedContainer.appendChild(iframe);
